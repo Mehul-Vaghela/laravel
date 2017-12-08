76f1064c6940169445663dc3f8a53d47a34c6129
Mv <vaghelamehul12345@gmail.com>
2017-12-08 10:13:52 +0530

    route changes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,4 +10,4 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-Route::get('/get_theme',['as'=>'get_theme','uses'=>'Website\WebsiteController@theme_home']);
+Route::get('/',['as'=>'homepage','uses'=>'Website\WebsiteController@index']);
